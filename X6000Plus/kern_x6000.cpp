4b5799cd1a5ccb4a9673a7bc6f9f0b9afd3d342d
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2023-07-25 15:42:55 +0300

    Add GetHWInfo wrap
    Wrap accelerator (X6000) GetHWInfo function to return supported card's device ID, which in turn allows VA2AcceleratorInfo::identify (AMDRadeonVA2Driver.bundle) to attach properly to the card and enable VCN support.

--- X6000Plus/kern_x6000.cpp
+++ X6000Plus/kern_x6000.cpp
@@ -2,3 +2,42 @@
 //  details.
 
 #include "kern_x6000.hpp"
+#include "kern_hwlibs.hpp"
+#include "kern_patterns.hpp"
+#include "kern_x6000p.hpp"
+#include <Headers/kern_api.hpp>
+#include <IOKit/IOService.h>
+
+static const char *pathRadeonX6000 = "/System/Library/Extensions/AMDRadeonX6000.kext/Contents/MacOS/AMDRadeonX6000";
+
+static KernelPatcher::KextInfo pathRadeonX6000 {"com.apple.kext.AMDRadeonX6000", &pathRadeonX6000, 1,
+    {}, {}, KernelPatcher::KextInfo::Unloaded};
+
+X6000 *X6000::callback = nullptr;
+
+void X6000::init() {
+    callback = this;
+    lilu.onKextLoadForce(&pathRadeonX6000);
+}
+
+bool X6000::processKext(KernelPatcher &patcher, size_t id, mach_vm_address_t slide, size_t size) {
+    if (pathRadeonX6000.loadIndex == id) {
+        X6000P::callback->setRMMIOIfNecessary();
+
+        RouteRequestPlus requests[] = {
+            {"__ZN35AMDRadeonX6000_AMDAccelVideoContext9getHWInfoEP13sHardwareInfo", orgGetHWInfo, wrapGetHWInfo},
+        };
+        PANIC_COND(!RouteRequestPlus::routeAll(patcher, id, requests, slide, size), "x6000", "Failed to route symbols");
+        DBGLOG("x6000", "Processed AMDRadeonX6000.kext");
+
+        return true;
+    }
+
+    return false;
+}
+
+void X6000::wrapGetHWInfo(IOService *accelVideoCtx, void *hwInfo) {
+    int ret = FunctionCast(wrapGetHWInfo, X6000::callback->orgGetHWInfo)(accelVideoCtx, hwInfo);
+    getMember<uint16_t>(hwInfo, 0x4) = X6000P::callback->chipType < ChipType::Navi23 ? 0x73BF : 0x73FF;
+    return ret;
+}
--- X6000Plus/kern_x6000.hpp
+++ X6000Plus/kern_x6000.hpp
@@ -3,5 +3,22 @@
 
 #ifndef kern_x6000_hpp
 #define kern_x6000_hpp
+#include "kern_amd.hpp"
+#include "kern_patcherplus.hpp"
+#include "kern_x6000p.hpp"
+#include <Headers/kern_util.hpp>
+#include <IOKit/IOService.h>
+
+class X6000 {
+    public:
+    static X6000 *callback;
+    void init();
+    bool processKext(KernelPatcher &patcher, size_t id, mach_vm_address_t slide, size_t size);
+
+    private:
+    mach_vm_address_t orgGetHWInfo {0};
+
+    static void wrapGetHWInfo(IOService *accelVideoCtx, void *hwInfo);
+};
 
 #endif /* kern_x6000_hpp */
